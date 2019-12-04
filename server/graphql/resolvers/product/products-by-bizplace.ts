64f71c53cc95e8c4898e75c385e3e475cda98285
ChrisLim <chrislim@hatiolab.com>
2019-12-04 19:27:54 +0800

    v0.9.1

--- server/graphql/resolvers/product/products-by-bizplace.ts
+++ server/graphql/resolvers/product/products-by-bizplace.ts
@@ -7,8 +7,6 @@ export const productsByBizplaceResolver = {
   async productsByBizplace(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
 
-    // convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
-
     const [items, total] = await getRepository(Product).findAndCount({
       ...convertedParams,
       relations: ['domain', 'bizplace', 'productRef', 'productOptions', 'creator', 'updater']
