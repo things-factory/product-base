1e35c03f60e99157d91b8a168a1cb88ab02f4139
leemyunggyu <jaylee@hatiolab.com>
2019-12-09 11:04:41 +0900

    Merge branch 'master' of https://github.com/things-factory/product-base into 1.0-alpha

--- server/graphql/resolvers/product/index.ts
+++ server/graphql/resolvers/product/index.ts
@@ -3,12 +3,14 @@ import { deleteProduct, deleteProductResolver } from './delete-product'
 import { deleteProducts, deleteProductsResolver } from './delete-products'
 import { productResolver } from './product'
 import { productsResolver } from './products'
+import { productsByBizplaceResolver } from './products-by-bizplace'
 import { updateMultipleProduct } from './update-multiple-product'
 import { updateProduct, updateProductResolver } from './update-product'
 
 export const Query = {
   ...productsResolver,
-  ...productResolver
+  ...productResolver,
+  ...productsByBizplaceResolver
 }
 
 export const Mutation = {
---
+++ server/graphql/resolvers/product/products-by-bizplace.ts
@@ -0,0 +1,17 @@
+import { Bizplace } from '@things-factory/biz-base'
+import { convertListParams, ListParam } from '@things-factory/shell'
+import { getRepository, In } from 'typeorm'
+import { Product } from '../../../entities'
+
+export const productsByBizplaceResolver = {
+  async productsByBizplace(_: any, params: ListParam, context: any) {
+    const convertedParams = convertListParams(params)
+
+    const [items, total] = await getRepository(Product).findAndCount({
+      ...convertedParams,
+      relations: ['domain', 'bizplace', 'productRef', 'productOptions', 'creator', 'updater']
+    })
+
+    return { items, total }
+  }
+}
