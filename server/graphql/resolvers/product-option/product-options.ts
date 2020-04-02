b2ead24ad3318a4b3aad4c182c5a4162d81c875b
ChrisLim <chrislim@hatiolab.com>
2020-04-02 16:29:49 +0800

    v2.4.0-beta.6

--- server/graphql/resolvers/product-option/product-options.ts
+++ server/graphql/resolvers/product-option/product-options.ts
@@ -1,14 +1,16 @@
+import { getPermittedBizplaceIds } from '@things-factory/biz-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository } from 'typeorm'
+import { getRepository, In } from 'typeorm'
 import { ProductOption } from '../../../entities'
 
 export const productOptionsResolver = {
   async productOptions(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
+    convertedParams.where.bizplace = In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
 
     const [items, total] = await getRepository(ProductOption).findAndCount({
       ...convertedParams,
-      relations: ['domain', 'creator', 'updater']
+      relations: ['domain', 'bizplace', 'creator', 'updater']
     })
 
     return { items, total }
