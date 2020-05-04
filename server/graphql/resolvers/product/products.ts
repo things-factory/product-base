60322d5e051c49dab0325eec8198f94efeadca05
leemyunggyu <jaylee@hatiolab.com>
2020-05-04 09:48:29 +0900

    Fix to use query builder

--- server/graphql/resolvers/product/products.ts
+++ server/graphql/resolvers/product/products.ts
@@ -1,18 +1,29 @@
 import { getPermittedBizplaceIds } from '@things-factory/biz-base'
-import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In } from 'typeorm'
+import { buildQuery, ListParam } from '@things-factory/shell'
+import { getRepository, SelectQueryBuilder } from 'typeorm'
 import { Product } from '../../../entities'
 
 export const productsResolver = {
   async products(_: any, params: ListParam, context: any) {
-    const convertedParams = convertListParams(params)
-    convertedParams.where.bizplace = In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
+    const qb: SelectQueryBuilder<Product> = getRepository(Product).createQueryBuilder()
 
-    const [items, total] = await getRepository(Product).findAndCount({
-      ...convertedParams,
-      relations: ['domain', 'bizplace', 'productRef', 'creator', 'updater']
-    })
+    params.filters = [
+      ...params.filters,
+      {
+        name: 'bizplace_id',
+        operator: 'in',
+        value: await getPermittedBizplaceIds(context.state.domain, context.state.user),
+      },
+    ]
+    buildQuery(qb, params, context)
+    const [items, total] = await qb
+      .leftJoinAndSelect('Product.domain', 'Domain')
+      .leftJoinAndSelect('Product.bizplace', 'Bizplace')
+      .leftJoinAndSelect('Product.productRef', 'ProductRef')
+      .leftJoinAndSelect('Product.creator', 'Creator')
+      .leftJoinAndSelect('Product.updater', 'Updater')
+      .getManyAndCount()
 
     return { items, total }
-  }
+  },
 }
