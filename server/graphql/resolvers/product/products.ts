a032cb7692019480e295039f12bb3dc1799cf6c4
moadmmh <moad.elhebri@live.iium.edu.my>
2020-09-09 12:39:41 +0800

    Added Multiple columns search
    Added Query builder for searching by SKU, name, description of the product

--- server/graphql/resolvers/product/products.ts
+++ server/graphql/resolvers/product/products.ts
@@ -1,21 +1,48 @@
 import { getPermittedBizplaceIds } from '@things-factory/biz-base'
-import { buildQuery, ListParam } from '@things-factory/shell'
-import { getRepository, SelectQueryBuilder } from 'typeorm'
+import { buildQuery, ListParam, buildCondition } from '@things-factory/shell'
+import { getRepository, SelectQueryBuilder, Brackets } from 'typeorm'
 import { Product } from '../../../entities'
 
 export const productsResolver = {
   async products(_: any, params: ListParam, context: any) {
     const qb: SelectQueryBuilder<Product> = getRepository(Product).createQueryBuilder()
 
+    const productFilters = params.filters.filter(x => x.name == 'product_info')
+
+    const filters = params.filters.filter(x => x.name != 'product_info')
+
+    const productFilterColumns = ['sku', 'name', 'description']
+
     params.filters = [
-      ...params.filters,
+      ...filters,
       {
         name: 'bizplace_id',
         operator: 'in',
         value: await getPermittedBizplaceIds(context.state.domain, context.state.user)
       }
     ]
+
     buildQuery(qb, params, context)
+    if (productFilters && productFilters.length > 0) {
+      let productInfo = productFilters[0]
+      qb.andWhere(
+        new Brackets(qb2 => {
+          productFilterColumns.forEach(filter => {
+            const condition = buildCondition(
+              qb.alias,
+              filter,
+              productInfo.operator,
+              productInfo.value,
+              productInfo.relation,
+              Object.keys(qb.getParameters()).length
+            )
+
+            qb2.orWhere(condition.clause, condition.parameters)
+          })
+        })
+      )
+    }
+
     const [items, total] = await qb
       .leftJoinAndSelect('Product.domain', 'Domain')
       .leftJoinAndSelect('Product.bizplace', 'Bizplace')
